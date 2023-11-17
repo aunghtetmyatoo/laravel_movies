6550581c72e6273b65d6411f18883e6e3ec86f4d
ahmo <aunghtetmyatoo@ucsy.edu.mm>
2023-11-17 15:06:07 +0630

    update imdb

--- resources/views/tv/show.blade.php
+++ resources/views/tv/show.blade.php
@@ -10,7 +10,7 @@
                 <h2 class="text-4xl font-semibold">{{ $tvShow['name'] }}</h2>
                 <div class="flex items-center text-gray-400 text-sm">
                     <svg class="mt-1 fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"></path></g></svg>
-                    <span class="mt-1">{{ $tvShow['vote_average'] }}</span>
+                    <span class="mt-1 ml-1">{{ round($tvShow['vote_average'], 1) }}</span>
                     <span class="mx-2">|</span>
                     <span>{{ $tvShow['first_air_date'] }}</span>
                     <span class="mx-2">|</span>
@@ -30,11 +30,11 @@
                         @endforeach
                     </div>
                 </div>
-                
+
                 <div x-data="{isOpen: false}">
                     @if(count($tvShow['videos']['results']) > 0)
                         <div class="mt-12">
-                            <button 
+                            <button
                                 @click="isOpen=true"
                                 class="bg-orange-500 text-gray-900 rounded flex inline-flex items-center font-semibold hover:bg-orange-600 px-5 py-4 transition ease-in-out duration-150"
                             >
@@ -44,9 +44,9 @@
                         </div>
                     @endif
 
-                    <div 
+                    <div
                         x-show.transition.opacity="isOpen"
-                        style="background-color: rgba(0, 0, 0, .5);" 
+                        style="background-color: rgba(0, 0, 0, .5);"
                         class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
                     >
                         <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
@@ -94,7 +94,7 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                 @foreach($tvShow['images'] as $backdrop)
                     <div class="mt-8">
-                        <a 
+                        <a
                             @click.prevent="
                                 isOpen=true
                                 image='{{ 'https://image.tmdb.org/t/p/original/'.$backdrop['file_path'] }}'
@@ -108,17 +108,17 @@
             </div>
         </div>
 
-        <div 
+        <div
             x-show.transition.opacity="isOpen"
-            style="background-color: rgba(0, 0, 0, .5);" 
+            style="background-color: rgba(0, 0, 0, .5);"
             class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
         >
             <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
                 <div class="bg-gray-900 rounded">
                     <div class="flex justify-end pr-4 pt-2">
-                        <button 
-                            @click="isOpen = false"    
-                            @keydown.escape.window="isOpen = false" 
+                        <button
+                            @click="isOpen = false"
+                            @keydown.escape.window="isOpen = false"
                             class="text-3xl leading-none hover:text-gray-300"
                         >×
                         </button>
@@ -130,5 +130,5 @@
             </div>
         </div>
     </div>
- 
+
 @endsection
