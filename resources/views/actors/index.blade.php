d16b11767dced4245f10b195dd507f67dad11492
ahmo <aunghtetmyatoo888@gmail.com>
2024-02-01 14:25:43 +0630

    update actor photo

--- resources/views/actors/index.blade.php
+++ resources/views/actors/index.blade.php
@@ -5,13 +5,15 @@
         <div class="popular-movies">
             <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Actors</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                @foreach($popularActors as $actor)
+                @foreach ($popularActors as $actor)
                     <div class="actor mt-8">
-                        <a href="{{ route('actors.show',$actor['id']) }}">
-                            <img src="{{ $actor['profile_path'] }}" alt="profile image" class="hover:opacity-75 transition ease-in-out duration-150">
+                        <a href="{{ route('actors.show', $actor['id']) }}">
+                            <img src="{{ $actor['profile_path'] }}" alt="profile image"
+                                class="w-full hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                         <div class="mt-2">
-                            <a href="{{ route('actors.show',$actor['id']) }}" class="text-lg hover:text-gray-300">{{ $actor['name'] }}</a>
+                            <a href="{{ route('actors.show', $actor['id']) }}"
+                                class="text-lg hover:text-gray-300">{{ $actor['name'] }}</a>
                         </div>
                         <div class="text-sm truncate text-gray-400">{{ $actor['known_for'] }}</div>
                     </div>
@@ -27,17 +29,17 @@
         </div>
 
         <!-- <div class="flex justify-between mt-16">
-            @if($previous)
-                <a href="/actors/page/{{ $previous }}">Previous</a>
-            @else
-                <div></div>
-            @endif
-            @if($next)
-                <a href="/actors/page/{{ $next }}">Next</a>
-            @else
-                <div></div>
-            @endif
-        </div> -->
+                @if ($previous)
+    <a href="/actors/page/{{ $previous }}">Previous</a>
+@else
+    <div></div>
+    @endif
+                @if ($next)
+    <a href="/actors/page/{{ $next }}">Next</a>
+@else
+    <div></div>
+    @endif
+            </div> -->
     </div>
 @endsection
 
@@ -45,11 +47,11 @@
     <script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script>
     <script>
         var elem = document.querySelector('.grid');
-        var infScroll = new InfiniteScroll( elem, {
-        path: '/actors/page/@{{#}}',
-        append: '.actor',
-        status: '.page-load-status'
-        // history: false,
-});
+        var infScroll = new InfiniteScroll(elem, {
+            path: '/actors/page/@{{ # }}',
+            append: '.actor',
+            status: '.page-load-status'
+            // history: false,
+        });
     </script>
 @endsection
